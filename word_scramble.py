c1cb48fa814091930c2c093c08f343ac96935941
ellechappelle <matheeli@oregonstate.edu>
2026-05-12 23:38:41 -0700

    Tasks 1 and 2

--- word_scramble.py
+++ word_scramble.py
@@ -39,23 +39,63 @@ def scramble_word(secret_word):
 # Task 1.1 
 def input_check(secret_word):
     # Remove 'pass', fill in your code here
-    pass
+    while True:
+        user_guess = input("Your guess: ")
+
+        guess_cleaned = ""
+
+        for letter in user_guess:
+            if letter.isalpha():
+               guess_cleaned += letter.lower()
+
+        sorted_guess = sorted(guess_cleaned)
+        if sorted_guess == sorted(secret_word):
+            return guess_cleaned
+        else:
+            print("Invalid input. Please use only the letters from the secret word.")
 
 # Task 1.2
 def has_player_won(secret_word, user_guess):
     # Remove 'pass', fill in your code here
-    pass
+    if user_guess == secret_word:
+        return True
+    else:
+        return False
 
 # Task 1.3 
 def get_word_progress(secret_word, user_guess):
     # Remove 'pass', fill in your code here
-    pass
+    letter_positions = ""
+    for i in range(0, len(secret_word)):
+        if user_guess[i] == secret_word[i]:
+            letter_positions += user_guess[i]
+        else:
+            letter_positions += '*'
+
+    return letter_positions
 
 # Task 2.1, 2.2 
 def word_scramble():
     word_list = load_words()
     # Remove 'pass', fill in your code here
-    pass
+    secret_word = choose_word(word_list)
+    print("Welcome to Word Scramble!")
+    scramble_word(secret_word)
+    print("You have 5 attempts to guess the original word.")
+
+    remaining_attempts = 5
+    while remaining_attempts > 0:
+        print()
+        user_guess = input_check(secret_word)
+        if has_player_won(secret_word, user_guess) is True:
+            print(f"Congratulations! You guess the word: {user_guess}")
+            return
+        else:
+            remaining_attempts -= 1
+            print(f"Incorrect. Progress: {get_word_progress(secret_word, user_guess)}")
+            print(f"Attempts left: {remaining_attempts}")
+
+    print(f"Sorry, you ran out of guesses. The word was {secret_word}")
 
 
 if __name__ == "__main__":
